7a2a9a899c303fea548d4ca0882dd65dd821500f
Meer Sagor <meersagor.contact@gmail.com>
2025-01-29 17:02:30 +0600

    chore: initial setup

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Landing page</h1>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <Button class="bg-neutral-800">Click me</Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
