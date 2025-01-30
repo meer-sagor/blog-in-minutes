3ebd711ee046b93a8c009ecfd8e8cef8e7ad2c37
Meer Sagor <meersagor.contact@gmail.com>
2025-01-30 21:23:18 +0600

    fix: remove lucid-vue-next pack and use-case

--- components/ui/dropdown-menu/DropdownMenuRadioItem.vue
+++ components/ui/dropdown-menu/DropdownMenuRadioItem.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { cn } from '@/utils'
-import { Circle } from 'lucide-vue-next'
 import {
   DropdownMenuItemIndicator,
   DropdownMenuRadioItem,
@@ -33,7 +32,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
   >
     <span class="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <DropdownMenuItemIndicator>
-        <Circle class="h-2 w-2 fill-current" />
+        <Icon name="lucide:check"/>
       </DropdownMenuItemIndicator>
     </span>
     <slot />
